df099cceafc69026615f223e1449f08545ac54be
chundawat-h <himanshuchundawat00@gmail.com>
2026-06-06 10:58:55 +0530

    deployment required changes

--- app.py
+++ app.py
@@ -226,4 +226,6 @@ def ratelimit_handler(e):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    debug = os.environ.get("FLASK_ENV", "production") == "development"
+    app.run(host="0.0.0.0", port=port, debug=debug)
